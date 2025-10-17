2a42cde98a22518a2e78b4933f0ce2a2ca3cc305
Farhan Daemi <fdmxfarhan@yahoo.com>
2025-10-17 11:24:24 +0330

    asdf

--- Light Weight/codes/01_TSOP1738_Board/TSOP1738_Board/TSOP1738_Board.ino
+++ Light Weight/codes/01_TSOP1738_Board/TSOP1738_Board/TSOP1738_Board.ino
@@ -11,9 +11,13 @@ void setup() {
   pinMode(PA6, OUTPUT);
   Wire.begin(I2C_ADDRESS);       
   Wire.onRequest(onRequest);
+  pinMode(PA4, OUTPUT);
+
 }
 
 void loop() {
+  digitalWrite(PA4, 1);
+  delay(1);
   for(int i=0; i<8; i++){
     digitalWrite(PB1, (i/4) % 2);
     digitalWrite(PA7, (i/2) % 2);
@@ -24,7 +28,6 @@ void loop() {
     // while(!digitalRead(PA0)) pulse++;
     pulse = pulseIn(PA0, LOW, 5000);
     i2cBuffer[i] = pulse;
-    
     start_time = 0;
     pulse = 0;
     // while(digitalRead(PA1) && start_time < 100) start_time++;
@@ -32,6 +35,9 @@ void loop() {
     pulse = pulseIn(PA1, LOW, 5000);
     i2cBuffer[8+i] = pulse;
   }
+  digitalWrite(PA4, 0);
+  delay(1);
+
 }
 
 void onRequest() {
--- Light Weight/codes/01_TSOP1738_Board/reciever/reciever.ino
+++ Light Weight/codes/01_TSOP1738_Board/reciever/reciever.ino
@@ -1,20 +1,25 @@
 #include <Wire.h>
-
+#include <Adafruit_SH1106_STM32.h>
+Adafruit_SH1106 display(-1);
 #define SLAVE_ADDR 0x09
 #define READ_LEN 16
-
+TwoWire i2c(2, I2C_FAST_MODE);
 void setup() {
-  Serial.begin(115200);
-  Wire.begin();  // Join I2C bus as master
+  // Serial.begin(115200);
+  display.begin(0x3c);
+  display.setTextColor(WHITE);
+  display.setTextSize(1);
+  display.display();
+  i2c.begin();  // Join I2C bus as master
 }
 
 void loop() {
-  Wire.requestFrom(SLAVE_ADDR, READ_LEN);
+  i2c.requestFrom(SLAVE_ADDR, READ_LEN);
   uint8_t index = 0;
   float sumX = 0, sumY = 0;
   uint8_t buffer[16];
-  while (Wire.available() && index < READ_LEN) {
-    uint8_t data = Wire.read();
+  while (i2c.available() && index < READ_LEN) {
+    uint8_t data = i2c.read();
     buffer[index] = data;
     float angleDeg = index * 22.5;
     float angleRad = angleDeg * (3.1415926 / 180.0);
@@ -25,12 +30,23 @@ void loop() {
   float angle = atan2(sumY, sumX) * 180.0 / 3.1415926;
   if (angle < 0) angle += 360.0;
 
-  // Serial.print("angle: ");
-  for (uint8_t i = 0; i < index; i++) {
-    Serial.print(buffer[i]);
-    Serial.print('\t');
+  display.clearDisplay();
+  display.setCursor(0, 0);
+  display.println(buffer[0]);
+  display.println(buffer[1]);
+  display.drawCircle(64, 32, 4, WHITE);
+  for(int i=0; i < 16; i++){
+    if(buffer[i] > 10 && buffer[i] < 1000){
+      int r = (12 + (buffer[i]/15.0));
+      display.fillCircle(65 +  r * sin(radians(i*22.5)), 32 - r * cos(radians(i*22.5)), 2, WHITE);
+    }
   }
-  Serial.print(angle);
-  Serial.println();
-  delay(100);
+  display.display();
+  // Serial.print("angle: ");
+  // for (uint8_t i = 0; i < index; i++) {
+    // Serial.print(buffer[i]);
+    // Serial.print('\t');
+  // }
+  // Serial.print(angle);
+  // Serial.println();
 }
